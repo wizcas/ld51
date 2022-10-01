d18818441c5113c4090f8da4b905dad2150ae591
Wizcas Chen <chen@0x1c.dev>
2022-10-01 09:56:40 +0800

    initial commit

---
+++ Player.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public class Player : KinematicBody2D
+{
+  #region Nested
+  #endregion
+
+  #region Signals
+  #endregion
+
+  #region Fields & Properties
+  #endregion
+
+  #region Hooks
+  public override void _Ready()
+  {
+
+  }
+  #endregion
+
+  #region Methods
+  #endregion
+}
