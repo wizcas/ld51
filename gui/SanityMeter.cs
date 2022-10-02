d5cf95351ab6a84b3318f53eb03d6d916428e501
Wizcas Chen <chen@0x1c.dev>
2022-10-02 16:47:50 +0800

    red sanity & gui layout update

--- gui/SanityMeter.cs
+++ gui/SanityMeter.cs
@@ -1,5 +1,13 @@
+using Godot;
 public class SanityMeter : ValueMeter
 {
+  private Color _normalColor;
+
+  public override void _Ready()
+  {
+    base._Ready();
+    _normalColor = TintProgress;
+  }
   protected override bool HasValue()
   {
     return Global.Instance.Player != null;
@@ -7,5 +15,14 @@ public class SanityMeter : ValueMeter
   protected override float GetMeterValue()
   {
     return Global.Instance.Player.Sanity.Sanity;
+  }
+
+  public override void _Process(float delta)
+  {
+    base._Process(delta);
+    if (Value < MaxValue * .3f)
+    {
+      TintProgress = Value <= MaxValue * .3f ? Colors.Red : _normalColor;
+    }
   }
 }
